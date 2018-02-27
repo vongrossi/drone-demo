5b57d371cf4eba833bb1ec85c868252bffe57c56
Fernando Barbosa <fbcbarbosa@gmail.com>
2018-02-27 00:33:18 -0300

    Fix typo in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import socket
 
 # Connect to Redis
-redis = Redis(host=os.getenv("REDIS_HOST", "redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host=os.getenv("REDIS_HOST", "redis"), db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
