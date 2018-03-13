4fd899af717f3c91c148fab2cae5ab2ecd57c395
Fernando Barbosa <fernando.barbosa@quintoandar.com.br>
2018-03-13 17:54:29 -0300

    Change name to estabilis

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello():
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3>Hello {name}!</h3>"            "<b>Hostname:</b> {hostname}<br/>"            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name="Estabilis", hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv("PORT", "4000")))
